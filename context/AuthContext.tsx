58bead3ed01709ecdb3a1192e56a54971e2c1910
matheus.martinez <matheus.martinez@uppertools.inf.br>
2021-09-10 07:55:43 -0300

    Criação de projeto autenticação

---
+++ context/AuthContext.tsx
@@ -0,0 +1,91 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+import Router from 'next/router';
+import { setCookie, parseCookies} from 'nookies'
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const {"reactauth.token": token} = parseCookies()
+
+    if (token){
+      api.get('/me').then(response => {
+        const { email, permissions, roles} = response.data
+
+        setUser({ email, permissions, roles})
+      })
+    }
+  }, [])
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      // maneiras de armazenar o token e o refreshToken
+      // session Storage - Se o usuário fechar o navegador e abrir novamente, perderá as informações.
+      // localStorage - Funciona somente no lado do cliente, como estamos usando o next (Server-Side) ficaria difícil usar esta opção.
+      // cookies - Pode ser acessado tanto no lado do servidor, como no lado do cliente. É a maneira que será utilizada.
+
+      // como o método de login "signIn" ocorre sempre pelo lado do servidor, o primeiro parâmetro do cookie deve ir como undefined.
+      // ao colocar o path somente com a '/', estou dizendo que a aplicação toda terá acesso a este cookie.
+      setCookie(undefined, 'reactauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      } )
+      setCookie(undefined, 'reactauth.refreshtoken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      // É necessário sempre atualizar o valor do token
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+      Router.push('/dashboard');
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, user, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
